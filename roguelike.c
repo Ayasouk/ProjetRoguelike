126483900028905475de4c0972242e874fe427d8
Oukache Ayas <spi2047@ic2s120-20.univ-lemans.fr>
2014-11-19 11:05:37 +0100

    creation du fichier roguelike.c

---
+++ roguelike.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 50
+
+void initialiser(){
+
+
+}
+
+
+void generer(){
+
+
+}
+
+void afficher(){
+
+
+}
+
+void deplacement(){
+
+
+}
+
+int gagne(){
+
+
+}
