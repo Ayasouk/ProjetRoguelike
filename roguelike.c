ef534c271b43e7ff184ee5bf5b3c5c2163238956
Hector Basset <hector.basset.etu@univ-lemans.fr>
2014-11-25 14:19:03 +0100

    Début d'implémentation

--- roguelike.c
+++ roguelike.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 50
 
-void initialiser(){
+#define TAILLE 30//ordre de la matrice
 
+typedef struct {
+    int ligne;
+    int column;
+} Coordonnee;
+
+typedef enum {
+    VIDE = ' ',
+    MUR = '#'
+} Case;
+
+Case labyrinthe[TAILLE][TAILLE];
+
+void initialiser() {
+}
+
+void generer() {
+    int i, j;
+    for (i = 0 ; i < TAILLE ; i++) {
+        for (j = 0 ; j < TAILLE ; j++) {
+            labyrinthe[i][j] = MUR;
+        }
+    }
+}
+
+void afficher() {
+    int i, j;
+    for (i = 0 ; i < TAILLE ; i++) {
+        for (j = 0 ; j < TAILLE ; j++) {
+            printf("%c", labyrinthe[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void deplacement() {
 
 }
 
-
-void generer(){
-
-
-}
-
-void afficher(){
-
-
-}
-
-void deplacement(){
-
-
-}
-
-int gagne(){
-
-
+int gagne() {
+    return 0;
 }
