a9a40f044f60d71b0e9f287b991d9c20507c9fca
Hector Basset <hector.basset.etu@univ-lemans.fr>
2014-12-04 15:02:37 +0100

    Amélioration de la documentation

--- src/roguelike.c
+++ src/roguelike.c
@@ -30,10 +30,10 @@ Dimension maze_dimension;
 Location player_location;
 
 void init_roguelike() {
-    srand(time(NULL));
-    maze = NULL;
-    maze_dimension.horizontal = maze_dimension.vertical = -1;
-    player_location.row = player_location.column = -1;
+	srand(time(NULL));
+	maze = NULL;
+	maze_dimension.horizontal = maze_dimension.vertical = -1;
+	player_location.row = player_location.column = -1;
 }
 
 void final_roguelike() {
@@ -86,34 +86,34 @@ void move_player(Direction direction) {
 /*Rectangle lab_dim = {0, 0, TAILLE - 1, TAILLE - 1};
 
 int random2(int a, int b) {
-    b +=1;
-    return rand() % (b - a) + a;
+	b +=1;
+	return rand() % (b - a) + a;
 }
 
 void generer() {//la fonction de génération devient la fonction generate_maze(), continuer ou recopier le code à l'intérieur
-    int i, j, n_salles, s;
-    Rectangle * salles;
-    for (i = 0 ; i < TAILLE ; i++) {
-        for (j = 0 ; j < TAILLE ; j++) {
-            labyrinthe[i][j] = MUR;
-        }
-    }
-    n_salles = random2(3, lab_dim.l2 / 5);
-    salles = (Rectangle *) malloc(n_salles * sizeof(Rectangle));
-    if (random2(0, 1)) {
-        salles[0].l1 = lab_dim.l1 + 1;
-        salles[0].c1 = random2(lab_dim.c1 + 1, lab_dim.c2 / 4);
-    } else {
-        salles[0].l1 = random2(lab_dim.l1 + 1, lab_dim.l2 / 4);
-        salles[0].c1 = lab_dim.c1 + 1;
-    }
-    salles[0].l2 = random2(salles[0].l1 + 4, salles[0].l1 + lab_dim.l2 / 3);
-    salles[0].c2 = random2(salles[0].c1 + 4, salles[0].c1 + lab_dim.c2 / 3);
-    for (s = 0 ; s <= 0 ; s++) {
-        for (i = salles[0].l1 ; i < salles[0].l2 ; i++) {
-            for (j = salles[0].c1 ; j < salles[0].c2 ; j++) {
-                labyrinthe[i][j] = VIDE;
-            }
-        }
-    }
+	int i, j, n_salles, s;
+	Rectangle * salles;
+	for (i = 0 ; i < TAILLE ; i++) {
+		for (j = 0 ; j < TAILLE ; j++) {
+			labyrinthe[i][j] = MUR;
+		}
+	}
+	n_salles = random2(3, lab_dim.l2 / 5);
+	salles = (Rectangle *) malloc(n_salles * sizeof(Rectangle));
+	if (random2(0, 1)) {
+		salles[0].l1 = lab_dim.l1 + 1;
+		salles[0].c1 = random2(lab_dim.c1 + 1, lab_dim.c2 / 4);
+	} else {
+		salles[0].l1 = random2(lab_dim.l1 + 1, lab_dim.l2 / 4);
+		salles[0].c1 = lab_dim.c1 + 1;
+	}
+	salles[0].l2 = random2(salles[0].l1 + 4, salles[0].l1 + lab_dim.l2 / 3);
+	salles[0].c2 = random2(salles[0].c1 + 4, salles[0].c1 + lab_dim.c2 / 3);
+	for (s = 0 ; s <= 0 ; s++) {
+		for (i = salles[0].l1 ; i < salles[0].l2 ; i++) {
+			for (j = salles[0].c1 ; j < salles[0].c2 ; j++) {
+				labyrinthe[i][j] = VIDE;
+			}
+		}
+	}
 }*/
